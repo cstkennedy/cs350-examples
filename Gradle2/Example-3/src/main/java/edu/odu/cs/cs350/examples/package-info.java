40764c93e0698d54b3feb519ebe07e68fa980b04
Thomas J. Kennedy <tkennedy@cs.odu.edu>
2018-03-29 12:56:47 -0400

    Final edits to Gradle2 Example

--- Gradle2/Example-3/src/main/java/edu/odu/cs/cs350/examples/package-info.java
+++ Gradle2/Example-3/src/main/java/edu/odu/cs/cs350/examples/package-info.java
@@ -1,5 +1,5 @@
 /**
- * A collection of Cs 350 Examples that includes
+ * A collection of CS 350 Examples that includes
  * Gradle, JUnit, and (in later examples) CI.
  */
 package edu.odu.cs.cs350.examples;
