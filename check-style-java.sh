85820c1263ee8abf49b07c9e560cf69005556553
Thomas J. Kennedy <tkennedy@cs.odu.edu>
2019-08-06 17:03:13 -0400

    Add java style check script

---
+++ check-style-java.sh
@@ -0,0 +1,6 @@
+CHECKSTYLE_JAR=/home/tkennedy/Courses/bin/checkstyle-8.8-all.jar
+
+
+for i in Java1 Java2 Java3 OfficeHoursExamples JUnit; do
+    java -jar ${CHECKSTYLE_JAR} -c checkstyle.xml $i* 2>&1 | sort > build/$i-style-check-java.txt
+done
