938562ef6a41a86df90b6693bf0d2f489e9ce9f3
Thomas J. Kennedy <tkennedy@cs.odu.edu>
2019-11-11 21:29:46 -0500

    Remove deprecated deploy.sh scripts

--- check-style-java.sh
+++
@@ -1,6 +0,0 @@
-CHECKSTYLE_JAR=/home/tkennedy/Courses/bin/checkstyle-8.8-all.jar
-
-
-for i in Java1 Java2 Java3 OfficeHoursExamples JUnit; do
-    java -jar ${CHECKSTYLE_JAR} -c checkstyle.xml $i* 2>&1 | sort | sed "s|`pwd`/||g" > build/$i-style-check-java.txt
-done
