cb47b9484f164caad3199f4cce9754f3257eb30e
Thomas J. Kennedy <tkennedy@cs.odu.edu>
2025-06-26 11:41:30 -0400

    Make second round of updates to Gradle 8.14.2

--- Office-Hours/Relative-Path-Java/src/test/java/edu/odu/cs/cs350/examples/TestPathDemo.java
+++ Office-Hours/Relative-Path-Java/src/test/java/edu/odu/cs/cs350/examples/TestPathDemo.java
@@ -1,27 +1,19 @@
 package edu.odu.cs.cs350.examples;
 
-import org.junit.FixMethodOrder;
-import org.junit.runners.MethodSorters;
-import org.junit.Test;
-import org.junit.Before;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
 
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
-
-import org.hamcrest.core.IsNull;
-
-import java.util.List;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Iterator;
-
-import java.util.stream.Stream;
-import static java.util.stream.Collectors.toList;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.Files;
+
 import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
@@ -34,20 +26,25 @@ import java.io.IOException;
  *
  * 1 Test per mutator
  */
-@FixMethodOrder(MethodSorters.NAME_ASCENDING)
-public class TestPathDemo
+@SuppressWarnings({
+    "PMD.AtLeastOneConstructor",
+    "PMD.JUnitTestContainsTooManyAsserts",
+    "PMD.JUnitAssertionsShouldIncludeMessage"
+})
+@TestMethodOrder(MethodOrderer.MethodName.class)
+class TestPathDemo
 {
-    @Before
+    @BeforeEach
     public void setUp()
     {
         // Placeholder
     }
 
     @Test
-    public void testRelativePath()
+    void testRelativePath()
     {
-        String[] pathSteps = {"src", "test", "resources", "testProgram1", "KeyboardInput.java"};
-        Path path = Paths.get("src", "test", "resources", "testProgram1", "KeyboardInput.java");
+        // String[] pathSteps = {"src", "test", "resources", "testProgram1", "KeyboardInput.java"};
+        final Path path = Paths.get("src", "test", "resources", "testProgram1", "KeyboardInput.java");
 
         // System.out.println(path.toString());
         // System.out.println(String.join(java.io.File.pathSeparator, pathSteps));
@@ -56,14 +53,17 @@ public class TestPathDemo
         BufferedReader aBuffer = null;
 
         try {
-            aBuffer = new BufferedReader(new FileReader(path.toString()));
+            aBuffer = Files.newBufferedReader(path);
         }
         catch (FileNotFoundException exc) {
             fail("File Could Not be Opened");
         }
+        catch (IOException exc) {
+            fail("File Could Not be read");
+        }
 
         try {
-            String firstLine = aBuffer.readLine();
+            final String firstLine = aBuffer.readLine();
             assertThat(firstLine, equalTo("import java.util.Scanner;"));
         }
         catch (IOException exc) {
